bd9cb4046477db9bdf7f3c5be46a6ac1cf3f8dee
zimbatm <zimbatm@zimbatm.com>
2019-07-22 15:26:55 +0200

    fix the deployment

--- main.go
+++ main.go
@@ -5,11 +5,12 @@ import (
 	"net/http"
 
 	"github.com/urfave/negroni"
+	"github.com/zimbatm/nar-serve/handler"
 )
 
 func main() {
 	n := negroni.Classic() // Includes some default middlewares
-	n.UseHandler(http.HandlerFunc(Handler))
+	n.UseHandler(http.HandlerFunc(handler.Handler))
 
 	addr := ":3000"
 	fmt.Println("Starting server on address", addr)
