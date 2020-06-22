6ec7c4787f7b7d5be0320919a774ebe267bb0142
zimbatm <zimbatm@zimbatm.com>
2020-06-23 00:34:11 +0200

    Revert "Revert "stream the directory listing""
    This reverts commit c56595ce08ec62019887909a15c41d30a5ac4e39.

--- api/unpack/index.go
+++ api/unpack/index.go
@@ -15,12 +15,12 @@ import (
 	"github.com/zimbatm/go-nix/src/nar"
 )
 
-var nixCache = libstore.HTTPBinaryCacheStore {
-	CacheURI: getEnv("NAR_CACHE_URI", "https://cache.nixos.org"),
-}
-
 // MountPath is where this handler is supposed to be mounted
 const MountPath = "/nix/store/"
+
+var nixCache = libstore.HTTPBinaryCacheStore{
+	CacheURI: getEnv("NAR_CACHE_URI", "https://cache.nixos.org"),
+}
 
 // Handler is the entry-point for @now/go as well as the stub main.go net/http
 func Handler(w http.ResponseWriter, req *http.Request) {
@@ -98,6 +98,7 @@ func Handler(w http.ResponseWriter, req *http.Request) {
 			case nar.TypeDirectory:
 				w.Header().Set("Content-Type", "text/html")
 				fmt.Fprintf(w, "<p>%s is a directory:</p><ol>", hdr.Name)
+				flush(w)
 				for {
 					hdr2, err := narReader.Next()
 					if err != nil {
@@ -125,6 +126,7 @@ func Handler(w http.ResponseWriter, req *http.Request) {
 					}
 
 					fmt.Fprintf(w, "<li><a href='%s'>%s</a></li>", filepath.Join(narinfo.StorePath, hdr2.Name), label)
+					flush(w)
 				}
 			case nar.TypeSymlink:
 				redirectPath := absSymlink(narinfo, hdr)
@@ -188,4 +190,12 @@ func absSymlink(narinfo *libstore.NarInfo, hdr *nar.Header) string {
 	}
 
 	return filepath.Join(narinfo.StorePath, filepath.Dir(hdr.Name), hdr.Linkname)
+}
+
+func flush(rw http.ResponseWriter) {
+	f, ok := rw.(http.Flusher)
+	if !ok {
+		panic("ResponseWriter is not a Flusher")
+	}
+	f.Flush()
 }
