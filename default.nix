9da723eceb3ee4541c79da69c2981338c2e89407
zimbatm <zimbatm@zimbatm.com>
2020-07-31 22:37:46 +0200

    fix vendorSha256

--- default.nix
+++ default.nix
@@ -3,5 +3,5 @@ pkgs.buildGoModule {
   pname = "nar-serve";
   version = "latest";
   src = pkgs.lib.cleanSource ./.;
-  vendorSha256 = "sha256-CwIawMbfajNe0Rf1CuzApapCP387iuoAMgTHVzVVgEE=";
+  vendorSha256 = "sha256-I+Ki5ZFIyOXDyAcS13G1r+VBW8+xM4tZIjT3lr3U9kA=";
 }
